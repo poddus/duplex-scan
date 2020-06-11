870818ba923bea9b4bd25f18e35e9e71c2e16976
Leopold von Seckendorff <leopold@seckendorff.com>
2020-06-11 14:58:52 +0200

    lint python script

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
+from argparse import ArgumentParser
 import os
-import itertools
 import logging
 import pdfrw
-from argparse import ArgumentParser
 
 logging.basicConfig(
     format='%(levelname)s:%(message)s',
@@ -13,36 +11,43 @@ logging.basicConfig(
 
 
 def parse_arguments():
-    parser = ArgumentParser(description='Sort the pages of a PDF file '
-        'which was created by scanning a duplex print.')
-    parser.add_argument('inputPath', help='path to a PDF file of a duplex scan.')
-    
+    parser = ArgumentParser(
+        description='Sort the pages of a PDF file '
+        'which was created by scanning a duplex print.'
+    )
+    parser.add_argument(
+        'inputPath',
+        help='path to a PDF file of a duplex scan.'
+    )
+
     return parser.parse_args()
+
 
 def main(inputPath):
     indata = pdfrw.PdfReader(inputPath)
-    
+
     inputFileBasename = os.path.basename(inputPath)
     outputPath = '{}-sorted{}'.format(
         os.path.splitext(inputFileBasename)[0],
         os.path.splitext(inputFileBasename)[1]
-        )
+    )
     logging.debug('{} is the output path.'.format(outputPath))
 
     outdata = pdfrw.PdfWriter(outputPath)
 
     if len(indata.pages) % 2 != 0:
-        raise RuntimeError('There are an uneven number of pages in the PDF file. '
+        raise RuntimeError(
+            'There are an uneven number of pages in the PDF file. '
             'Something must have gone wrong during the scan. Please check the '
-            'input file and try again.')
+            'input file and try again.'
+        )
 
     allPages = len(indata.pages)
     halfPages = allPages // 2
     logging.debug('{} is the number of pages.'.format(allPages))
     logging.debug('{} is half the number of pages.'.format(halfPages))
 
-
-    oddPages = list(range(0, halfPages))    
+    oddPages = list(range(0, halfPages))
     logging.debug('{} are the odd pages in order.'.format(oddPages))
 
     evenPages = list(reversed(range(halfPages, allPages)))
@@ -59,6 +64,7 @@ def main(inputPath):
         outdata.addpage(indata.pages[page])
 
     outdata.write()
+
 
 if __name__ == '__main__':
     args = parse_arguments()
