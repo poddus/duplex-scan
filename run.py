65d1fce51d62aaa235c6017cf7d0f0578c60765b
Leopold von Seckendorff <leopold@seckendorff.com>
2021-07-26 21:06:59 +0200

    hack in the ability to handle one or two input paths, depending on the scan type
    remove silly logging
    change naming convention to be PIP compliant

--- run.py
+++ run.py
@@ -2,70 +2,78 @@
 
 from argparse import ArgumentParser
 import os
-import logging
 import pdfrw
-
-logging.basicConfig(
-    format='%(levelname)s:%(message)s',
-    level=logging.INFO)
-
 
 def parse_arguments():
     parser = ArgumentParser(
         description='Sort the pages of a PDF file '
-        'which was created by scanning a duplex print.'
+        'which was created by scanning a duplex print. '
+        'output path is the path of the PDF file containing the odd pages'
     )
     parser.add_argument(
-        'inputPath',
-        help='path to a PDF file of a duplex scan.'
+        'input_paths',
+        nargs='+',
+        help='Path(s) to PDF file(s) to be processed. Input either two files, odd pages first, even pages second '
+             'or a single file with both.'
     )
-
     return parser.parse_args()
 
 
-def main(inputPath):
-    indata = pdfrw.PdfReader(inputPath)
+def main(input_paths):
+    if len(input_paths) > 2:
+        raise ValueError('program only accepts 1 or 2 files')
 
-    inputFileBasename = os.path.basename(inputPath)
-    outputPath = '{}-sorted{}'.format(
-        os.path.splitext(inputFileBasename)[0],
-        os.path.splitext(inputFileBasename)[1]
+    input_file_basename = os.path.basename(input_paths[0])
+    output_path = '{}-sorted{}'.format(
+        os.path.splitext(input_file_basename)[0],
+        os.path.splitext(input_file_basename)[1]
     )
-    logging.debug('{} is the output path.'.format(outputPath))
+    out_data = pdfrw.PdfWriter(output_path)
 
-    outdata = pdfrw.PdfWriter(outputPath)
+    if len(input_paths) == 2:
+        odd_data = pdfrw.PdfReader(input_paths[0])
+        even_data = pdfrw.PdfReader(input_paths[1])
 
-    if len(indata.pages) % 2 != 0:
-        raise RuntimeError(
-            'There are an uneven number of pages in the PDF file. '
-            'Something must have gone wrong during the scan. Please check the '
-            'input file and try again.'
-        )
+        if len(odd_data.pages) != len(even_data.pages):
+            raise RuntimeError(
+                'The number of pages in the input files are unequal. '
+                'Something probable went wrong during the scan. Please check the '
+                'input files and try again.'
+            )
 
-    allPages = len(indata.pages)
-    halfPages = allPages // 2
-    logging.debug('{} is the number of pages.'.format(allPages))
-    logging.debug('{} is half the number of pages.'.format(halfPages))
+        current_even_page = list(range(0, len(even_data.pages)))
+        for current_odd_page in range(0, len(odd_data.pages)):
+            out_data.addpage(odd_data.pages[current_odd_page])
+            out_data.addpage((even_data.pages[current_even_page.pop()]))  # in practice .pop() reverses the list
 
-    oddPages = list(range(0, halfPages))
-    logging.debug('{} are the odd pages in order.'.format(oddPages))
+        out_data.write()
+    elif len(input_paths) == 1:
+        in_data = pdfrw.PdfReader(input_paths[0])
 
-    evenPages = list(reversed(range(halfPages, allPages)))
-    logging.debug('{} are the even pages in order.'.format(evenPages))
+        if len(in_data.pages) % 2 != 0:
+            raise RuntimeError(
+                'There are an uneven number of pages in the PDF file. '
+                'Something must have gone wrong during the scan. Please check the '
+                'input file and try again.'
+            )
 
-    sortedPages = []
-    for page in range(0, halfPages):
-        sortedPages.append(oddPages[page])
-        sortedPages.append(evenPages[page])
+        all_pages = len(in_data.pages)
+        half_pages = all_pages // 2
+        odd_pages = list(range(0, half_pages))
 
-    logging.debug('{} is the list of sorted pages.'.format(sortedPages))
+        even_pages = list(reversed(range(half_pages, all_pages)))
 
-    for page in sortedPages:
-        outdata.addpage(indata.pages[page])
+        sorted_pages = []
+        for page in range(0, half_pages):
+            sorted_pages.append(odd_pages[page])
+            sorted_pages.append(even_pages[page])
 
-    outdata.write()
+        for page in sorted_pages:
+            out_data.addpage(in_data.pages[page])
+
+        out_data.write()
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    main(args.inputPath)
+    main(args.input_paths)
